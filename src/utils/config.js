03e09167ed63662cf202eae1a2612716ed4be7fd
Titouan Pellerin <titouan.pellerin@gmail.com>
2023-03-16 11:33:05 +0100

    fx debug build

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const debug = import.meta.env.OLYMPIC_DEBUG === 'true';
-// const env = import.meta.env.MODE;
+const env = import.meta.env.MODE;
 const url = new URLSearchParams(window.location.search);
 
-const DEBUG = url.has('debug') && debug;
+const DEBUG = env === 'development' ? url.has('debug') && debug : debug;
 
 const BREAKPOINTS = {
 	tablet: 768,
