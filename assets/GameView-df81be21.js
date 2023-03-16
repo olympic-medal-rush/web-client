a8b08500a68bd5a9a206a2aefe8816cda8a5ab2d
Maelle05 <56165289+Maelle05@users.noreply.github.com>
2023-03-16 15:52:56 +0000

    Deploying to gh-pages from @ olympic-medal-rush/web-client@cc21b6084a80ff518fe1b6e05655e2df1ba858f5 🚀

--- assets/GameView-df81be21.js
+++
@@ -1,1 +0,0 @@
-import{u as s,o as t,a as o,b as n,c,d as e,t as l,e as r}from"./index-2fffcbaa.js";const _={class:"game"},i=e("h1",null,"Game view",-1),h={__name:"GameView",setup(p){const{t:a}=s();return t(()=>o.load()),(u,d)=>(n(),c("div",_,[i,e("p",null,l(r(a)("head.title")),1)]))}};export{h as default};
