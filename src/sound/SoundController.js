dd01bd029d3df8f18a71aa8f4a07f41141e49a48
Maelle05 <maelle.rabouan@gmail.com>
2023-04-11 11:16:08 +0200

    re

--- src/sound/SoundController.js
+++ src/sound/SoundController.js
@@ -4,22 +4,19 @@ import { manifest } from '@webglApp/utils/manifest';
 import { EVENTS } from '@utils/constants';
 import SingleAudio from './SingleAudio';
 
-let iSoundController;
 class SoundController {
 	constructor() {
-		if (iSoundController) return this;
-
 		state.on(EVENTS.POINTER_DOWN, this.#initAudio);
 	}
 
 	#initAudio = () => {
 		this.audioCtx = new AudioContext();
-		state.off(EVENTS.POINTER_DOWN, this.#initAudio);
 
 		this.keys = Object.keys(manifest.sounds);
 		this.sounds = app.core.assetsManager.get(...this.keys);
 
 		this.audios = Object.fromEntries(this.keys.map((key, i) => [key, new SingleAudio(this.audioCtx, this.sounds[i])]));
+		state.off(EVENTS.POINTER_DOWN, this.#initAudio);
 	};
 
 	play(key) {
