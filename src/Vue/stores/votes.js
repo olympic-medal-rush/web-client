cb7a66952e303002a228aa74f2e1eaa245e93440
Maelle05 <maelle.rabouan@gmail.com>
2023-06-20 16:48:21 +0200

    feat(): Main arows annim

--- src/Vue/stores/votes.js
+++ src/Vue/stores/votes.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useVotesStore = defineStore('votes', {
 	state: () => {
-		return { up: 0, right: 0, down: 0, left: 0 };
+		return { up: 0, right: 0, down: 0, left: 0, time: 3800 };
 	},
 	actions: {
 		/**
@@ -25,6 +25,18 @@ export const useVotesStore = defineStore('votes', {
 
 		getSommeVote() {
 			return this.up + this.right + this.down + this.left;
+		},
+
+		updateTime({ dt }) {
+			this.time = this.time - dt * 1000;
+		},
+
+		resetTime() {
+			this.time = 3800;
+		},
+
+		getLeftTime() {
+			return Math.floor(this.time / 1000);
 		},
 	},
 });
