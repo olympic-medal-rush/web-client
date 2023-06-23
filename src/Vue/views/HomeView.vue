71ea090ccd0c74b5de3e659da1e04ecb17e615cb
Titouan Pellerin <titouan.pellerin@gmail.com>
2023-06-23 18:31:04 +0200

    fx

--- src/Vue/views/HomeView.vue
+++ src/Vue/views/HomeView.vue
@@ -1,21 +1,13 @@
 <script setup>
-<<<<<<< HEAD
-import { app } from '@/App';
-import router from '@Vue/router';
-=======
 import ButtonOrLink from '@components/Inputs/ButtonOrLink.vue';
->>>>>>> develop
 import TheLogo from './../components/TheLogo.vue';
 </script>
 
 <template>
 	<div class="home">
 		<TheLogo />
-<<<<<<< HEAD
-=======
 
 		<ButtonOrLink class="cta" to="/login"> Jouer </ButtonOrLink>
->>>>>>> develop
 	</div>
 </template>
 
